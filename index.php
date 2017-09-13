76e3e84abcef0a6851af0d0107f8165d856048bd
Tasnimul H. Tauhid <me@tauhid.xyz>
2017-09-14 02:25:55 +0600

    Update index.php
    Now the code checks Database for duplicate string & URL

--- index.php
+++ index.php
@@ -8,36 +8,49 @@ if (isset($_POST['url']) and !empty($_POST['url'])) {
 	$url	=	$_POST['url'];
 
 
-//string generator
-function string($len = 5){
-  $charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  $base = strlen($charset);
-  $result = '';
+	//string generator
+	function string($len = 5){
+	  $charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	  $base = strlen($charset);
+	  $result = '';
 
-  $now = explode(' ', microtime())[1];
-  while ($now >= $base){
-    $i = $now % $base;
-    $result = $charset[$i] . $result;
-    $now /= $base;
-  }
-  return substr($result, -5);
+	  $now = explode(' ', microtime())[1];
+	  while ($now >= $base){
+	    $i = $now % $base;
+	    $result = $charset[$i] . $result;
+	    $now /= $base;
+	  }
+	  return substr($result, -5);
+	}
+
+
+
+	//time now
+	$time	=	date("d/m/Y");
+
+	//Check for duplicate URL & String
+	$url_check	=	"SELECT url FROM data WHERE url = '$url'";
+	$url_check_processor	=	mysqli_query($dbconnect, $url_check);
+
+	$duplicate_string_check	=	"SELECT string FROM data WHERE string = '$thestring'";
+	$duplicate_string_check_processor =	mysqli_query($dbconnect, $duplicate_string_check);
+
+	if ($url_check_processor->num_rows) {
+		//If URL Exists - get the string for coresponding URL
+		$get_string	=	"SELECT string FROM data WHERE url = '$url'";
+		$get_string_processor	=	mysqli_query($dbconnect, $get_string);
+		$thestring	=	$get_string_processor->fetch_object()->string;
+		}	elseif ($duplicate_string_check_processor->num_rows) {
+			//If the string exists - get the string
+			$thestring	=	$duplicate_string_check_processor->fetch_object()->string;
+		}	else {
+			//Create New String
+			$thestring	=	string();
+			//Insert Data into DB
+			$query	=	"INSERT INTO data (url, string, time) VALUES ('{$url}', '{$thestring}', '{$time}')";
+			mysqli_query($dbconnect, $query) or die('Something happened!');
+		}
 }
-
-//get the string
-$thestring	=	string();
-
-//time now
-$time	=	date("d/m/Y");
-
-//check for duplicate string
-$check_query	=	"";
-
-//insert data into database
-$query	=	"INSERT INTO data (url, string, time) VALUES ('{$url}', '{$thestring}', '{$time}')";
-
-mysqli_query($dbconnect, $query) or die('Something happened!');
-}
-
 ?>
 <!DOCTYPE html>
 <html>
